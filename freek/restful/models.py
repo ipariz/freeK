4b1dc66bebe5989ccc1499389cd4c2c3425d788a
iman <iman.parhizkari@gmail.com>
2018-12-20 01:02:30 +0330

    pizza and topping model created

--- freek/restful/models.py
+++ freek/restful/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Topping(models.Model):
+    soda = models.BooleanField()
+    salad = models.BooleanField()
+    yogurt = models.BooleanField()
+
+class Pizza(models.Model):
+    type = models.CharField(max_length=40)
+    toppings = models.ManyToManyField(Topping)
