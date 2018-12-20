7802b2cf6932708b4ee58301d9627d1c3e04c8cc
iman <iman.parhizkari@gmail.com>
2018-12-20 18:22:24 +0330

    pizza and topping viewsets added

--- freek/restful/views.py
+++ freek/restful/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from restful.models import Pizza, Topping
+from restful.serializers import PizzaSerializer, ToppingSerializer
+from rest_framework import viewsets
 
-# Create your views here.
+class PizzaViewSet(viewsets.ModelViewSet):
+    serializer_class = PizzaSerializer
+    queryset = Pizza.objects.all()
+
+class ToppingViewSet(viewsets.ModelViewSet):
+    serializer_class = ToppingSerializer
+    queryset = Topping.objects.all()
+    
