70922b736cdf4eacbbff24e393d052ae1dad5449
iman <iman.parhizkari@gmail.com>
2018-12-30 17:01:23 +0330

    add LogViewSet class to views

--- freek/log/views.py
+++ freek/log/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from log.models import Log
+from log.serializers import LogSerializer
 
-# Create your views here.
+class LogViewSet(viewsets.ModelViewSet):
+    serializer_class = LogSerializer
+    queryset = Log.objects.all()
