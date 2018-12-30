133408d7e12890203da895a254e858b592c0ad2c
iman <iman.parhizkari@gmail.com>
2018-12-30 17:11:27 +0330

    add LogMiddleware to settings.MIDDLEWARE

--- freek/freek/settings.py
+++ freek/freek/settings.py
@@ -50,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'log.middleware.LogMiddleware',
 ]
 
 ROOT_URLCONF = 'freek.urls'
